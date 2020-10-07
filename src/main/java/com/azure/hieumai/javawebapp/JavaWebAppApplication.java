903e89026e7f891e287657db4b9ef3f47453b7ce
Hieu Mai <duyhieu009@yahoo.com>
2020-10-08 00:31:28 +0700

    Init code base

---
+++ src/main/java/com/azure/hieumai/javawebapp/JavaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.azure.hieumai.javawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/hieumai/javawebapp/JavaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.hieumai.javawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
